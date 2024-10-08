f80d00b364d0bfbdac192428a2202a41c8121050
Aram <aramhrptn@hotmail.com>
2024-10-08 21:29:11 +0400

    Update dev scripts and home spec to improve setup and tests

--- bin/web.sh
+++ bin/web.sh
@@ -2,4 +2,4 @@
 
 docker compose build
 docker compose run --rm web bundle exec rails db:prepare
-docker compose run --rm --tty web bundle exec rails s -p 3000 -b 0.0.0.0
+docker compose run --rm -p 3000:3000 --tty web bundle exec foreman start -f Procfile.dev
