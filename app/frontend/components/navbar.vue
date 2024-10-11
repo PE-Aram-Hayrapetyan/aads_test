a1429267c3d69bff80ec3fffe73f68337869b37d
Aram <aramhrptn@hotmail.com>
2024-10-11 23:34:27 +0400

    Add navbar component and integrate with App.vue

--- app/frontend/components/navbar.vue
+++ app/frontend/components/navbar.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
+
 import {defineProps} from 'vue'
 import UserType from "~/types/UserType";
-import Navbar from "~/components/navbar.vue";
 
 const props = defineProps<{
   user: UserType
 }>()
-
 </script>
 
 <template>
-  <navbar user={{props.user}} />
+  <p>navbar</p>
+  <p> {{ props.user.id }} </p>
+
 </template>
 
 <style scoped>
+
 </style>
