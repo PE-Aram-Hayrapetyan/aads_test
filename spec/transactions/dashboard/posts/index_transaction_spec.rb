5cc461fed8bdf9f2c30ccae7550fbad2593d66ff
Aram <aramhrptn@hotmail.com>
2024-10-11 19:13:20 +0400

    Add tests for user not found in posts transaction

--- spec/transactions/dashboard/posts/index_transaction_spec.rb
+++ spec/transactions/dashboard/posts/index_transaction_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Dashboard::Posts::IndexTransaction do
+  subject(:posts) { described_class.call(input) }
+
   let(:user) { create(:user) }
   let(:following) { create(:user_friends_relation, user:, other_user: create(:user)) }
   let(:user_posts) do
@@ -11,9 +13,9 @@ RSpec.describe Dashboard::Posts::IndexTransaction do
   end
 
   describe '#call' do
-    subject(:posts) { described_class.call(user_id: user.id) }
-
     context 'when user exists' do
+      let(:input) { { user_id: user.id } }
+
       before do
         following
       end
@@ -36,6 +38,18 @@ RSpec.describe Dashboard::Posts::IndexTransaction do
         user_posts
         post = posts.success.first
         expect(post.comments_count).to eq(Post.find(post.id).comments.count)
+      end
+    end
+
+    context 'when user does not exist' do
+      let(:input) { { user_id: SecureRandom.uuid } }
+
+      it 'returns failure' do
+        expect(posts).to be_failure
+      end
+
+      it 'returns error message' do
+        expect(posts.failure[:user_id]).to eq(['is invalid'])
       end
     end
   end
